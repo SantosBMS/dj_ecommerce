77087a1234c98f63b7ab618b52ea53a3c669d84f
SantosBMS <santosbms93@gmail.com>
2023-08-24 08:12:19 -0300

    modified:ecommerce/__pycache__/urls.cpython-38.pyc-products/views.py

--- products/views.py
+++ products/views.py
@@ -9,6 +9,7 @@ class ProductFeaturedListView(ListView):
     template_name = "products/list.html"
     
     def get_queryset(self, *args, **kwargs):
+        request = self.request
         return Product.objects.featured()
 
 class ProductFeaturedDetailView(DetailView):
@@ -20,11 +21,11 @@ class ProductListView(ListView):
     #traz todos os produtos do banco de dados sem filtrar nada 
     queryset = Product.objects.all()
     template_name = "products/list.html"
-    
-    #def get_context_data(self, *args, **kwargs):
-        #context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
-        #print(context)
-        #return context
+
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super(ProductListView, self).get_context_data(*args, **kwargs)
+    #     print(context)
+    #     return context
 
 #Function Based View
 def product_list_view(request):
@@ -34,16 +35,31 @@ def product_list_view(request):
     }
     return render(request, "products/list.html", context)
 
+class ProductDetailSlugView(DetailView):
+    queryset = Product.objects.all()
+    template_name = "products/detail.html"
+
+    def get_object(self, *args, **kwargs):
+        slug = self.kwargs.get('slug')
+        #instance = get_object_or_404(Product, slug = slug, active = True)
+        try:
+            instance = Product.objects.get(slug = slug, active = True)
+        except Product.DoesNotExist:
+            raise Http404("Não encontrado!")
+        except Product.MultipleObjectsReturned:
+            qs = Product.objects.filter(slug = slug, active = True)
+            instance =  qs.first()
+        return instance
+
 #Class Based View
 class ProductDetailView(DetailView):
-    #queryset = Product.objects.all()
     template_name = "products/detail.html"
 
     def get_context_data(self, *args, **kwargs):
         context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
         print(context)
         return context
-    
+
     def get_object(self, *args, **kwargs):
         pk = self.kwargs.get('pk')
         instance = Product.objects.get_by_id(pk)
