d35f84a7ff7625452031fecbd75c9bab50964ac0
SantosBMS <santosbms93@gmail.com>
2023-09-16 07:55:58 -0300

    Aula 40- exercicios- branch sessions

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, redirect
 from .forms import ContactForm, LoginForm, RegisterForm
 
 def home_page(request):
+    print(request.session.get('first_name', 'Unknow'))
     context = {
                     "title": "Página Sobre",
                     "content": "Bem vindo a Página sobre",
