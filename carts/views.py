9ef56a8c5edceaac297cf5040f1350cf7f900d95
SantosBMS <santosbms93@gmail.com>
2023-10-20 07:24:11 -0300

    Aula 58-- branch: associate_billing_profile_to_order

--- carts/views.py
+++ carts/views.py
@@ -8,7 +8,7 @@ from products.models import Product
 from .models import Cart
 
 def cart_home(request):
-    cart_obj, new_obj = Cart.objects.new_or_get(request)
+    cart_obj, new_obj  = Cart.objects.new_or_get(request)
     return render(request, "carts/home.html", {"cart": cart_obj})
 
 def cart_update(request):
@@ -36,24 +36,32 @@ def checkout_home(request):
     #ou se o carrinho já existir mas não tiver nada dentro
     if cart_created or cart_obj.products.count() == 0:
         return redirect("cart:home")
-
     #aqui a order associada ao carrinho
-    else:
-        order_obj, new_order_obj = Order.objects.get_or_create(cart = cart_obj)
     user = request.user
     billing_profile = None
     login_form = LoginForm()
     guest_form = GuestForm()
     guest_email_id = request.session.get('guest_email_id')
     if user.is_authenticated:
-        billing_profile, billing_profile_created = BillingProfile.objects.get_or_create(user=user, email=user.email)
+        billing_profile, billing_profile_created = BillingProfile.objects.get_or_create(
+            user=user, email=user.email)
     elif guest_email_id is not None:
         guest_email_obj = GuestEmail.objects.get(id=guest_email_id)
         billing_profile, billing_guest_profile_created = BillingProfile.objects.get_or_create(
-                                        email=guest_email_obj.email)
+            email=guest_email_obj.email)
     else:
         pass
 
+    if billing_profile is not None:
+        order_qs = Order.objects.filter(billing_profile = billing_profile, cart = cart_obj, active = True)
+        if order_qs.count() == 1:
+            order_obj = order_qs.first()
+        else:
+            old_order_qs = Order.objects.exclude(billing_profile = billing_profile).filter(cart = cart_obj, active = True)
+            if old_order_qs.exists():
+                old_order_qs.update(active = False)
+            order_obj = Order.objects.create(billing_profile = billing_profile, cart = cart_obj)
+    
     context = {
         "object": order_obj,
         "billing_profile": billing_profile,
--- orders/models.py
+++ orders/models.py
@@ -1,8 +1,9 @@
 import math
 from django.db import models
-from ecommerce.utils import unique_order_id_generator
 from django.db.models.signals import pre_save, post_save
 from carts.models import Cart
+from billing.models import BillingProfile
+from ecommerce.utils import unique_order_id_generator
 
 ORDER_STATUS_CHOICES = (
     ('created', 'Criado'),
@@ -12,6 +13,7 @@ ORDER_STATUS_CHOICES = (
 )
 
 class Order(models.Model):
+    billing_profile = models.ForeignKey(BillingProfile, on_delete=models.CASCADE, null = True, blank = True)
     order_id = models.CharField(max_length = 120, blank = True)
     # billing_profile = ?
     # shipping_address = ?
@@ -20,20 +22,19 @@ class Order(models.Model):
     status = models.CharField(max_length = 120, default = 'created', choices = ORDER_STATUS_CHOICES )
     shipping_total = models.DecimalField(default = 5.99, max_digits = 100, decimal_places = 2)
     total = models.DecimalField(default = 0.00, max_digits = 100, decimal_places = 2)
-
+    active = models.BooleanField(default=True)
+    
     def __str__(self):
         return self.order_id
-    
     def update_total(self):
         cart_total = self.cart.total
         shipping_total = self.shipping_total
-        #new_total = cart_total + shipping_total
         new_total = math.fsum([cart_total, shipping_total])
         formatted_total = format(new_total, '.2f')
         self.total = formatted_total 
         self.save()
         return new_total
-    
+
 def pre_save_create_order_id(sender, instance, *args, **kwargs):
     if not instance.order_id:
         instance.order_id = unique_order_id_generator(instance)
