099c03fa9038bd181bdb354f006757dec50ceb34
SantosBMS <santosbms93@gmail.com>
2023-10-28 07:20:15 -0300

    Aula 62-- branch: associate_addresses_to_order

--- carts/views.py
+++ carts/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render, redirect
 
 from accounts.forms import LoginForm, GuestForm
 from accounts.models import GuestEmail
+
 from addresses.forms import AddressForm
+from addresses.models import Address
+
 from billing.models import BillingProfile
 from orders.models import Order
 from products.models import Product
@@ -31,24 +34,34 @@ def cart_update(request):
 
 def checkout_home(request):
     #aqui a gente pega o carrinho
-    cart_obj, cart_created= Cart.objects.new_or_get(request)
+    cart_obj, cart_created = Cart.objects.new_or_get(request)
     order_obj = None
     #se o carrinho acabou de ser criado, ele tá zerado
     #ou se o carrinho já existir mas não tiver nada dentro
     if cart_created or cart_obj.products.count() == 0:
-        return redirect("cart:home")
+        return redirect("cart:home")  
+    
     login_form = LoginForm()
     guest_form = GuestForm()
     address_form = AddressForm()
-
+    billing_address_id = request.session.get("billing_address_id", None)
+    shipping_address_id = request.session.get("shipping_address_id", None)
     billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
     if billing_profile is not None:
         order_obj, order_obj_created = Order.objects.new_or_get(billing_profile, cart_obj)
-
+        if shipping_address_id:
+            order_obj.shipping_address = Address.objects.get(id = shipping_address_id)
+            del request.session["shipping_address_id"]
+        if billing_address_id:
+            order_obj.billing_address = Address.objects.get(id = billing_address_id) 
+            del request.session["billing_address_id"]
+        if billing_address_id or shipping_address_id:
+            order_obj.save()
     context = {
         "object": order_obj,
         "billing_profile": billing_profile,
         "login_form": login_form,
-        "guest_form": guest_form
+        "guest_form": guest_form,
+        "address_form": address_form,
     }
     return render(request, "carts/checkout.html", context)
